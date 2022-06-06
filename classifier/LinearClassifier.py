192383304106e334a8b146ac0f3c44a1d0a9ce6f
조유림 <whdbfla6@gmail.com>
2022-06-06 21:49:43 +0900

    linear classifier
    svm loss cross entropy loss

---
+++ classifier/LinearClassifier.py
@@ -0,0 +1,81 @@
+import torch
+
+class LinearClassifier:
+    def __init__(self):
+        self.W = None
+
+    def forward(
+            self,
+            X:torch,
+            y:torch,
+            lr: float = 0.01,
+            reg : float = 1e-5,
+            num_iters: int = 100
+            ):
+        n,c = X.shape
+        num_class = len(torch.unique(y))
+        if self.W == None: self.W = torch.rand((c,num_class))*0.001
+        
+        for _ in range(num_iters):
+            out = torch.mm(X,self.W)
+            loss,dW = self.loss(X,y,out,reg)
+            self.W -= dW
+
+    def pred(
+        self,
+        X,
+        ):
+
+        pred = torch.mm(X,self.W)
+        pred = torch.argmax(pred,axis=1)
+        return pred
+
+class LinearSVM(LinearClassifier):
+    def __init__(self):
+        super().__init__()
+    def loss(
+        self,
+        X,
+        y,
+        out,
+        reg : float = 1e-5,
+        ):
+
+        n,c = out.shape
+        sy = out[range(n),y].reshape((-1,1)).expand((-1,c))
+
+        margin = out-sy+1
+        margin[range(n),y] = 0
+        margin[margin<0] = 0
+        loss = torch.sum(margin)/n + reg*torch.sum(self.W.mul(self.W))
+
+        margin_copy = margin.clone()
+        margin_copy[margin_copy>0] = 1
+        margin_copy[range(n),y] = -torch.sum(margin_copy,axis=1)
+        dW = torch.mm(X.T,margin_copy)/n
+        dW += 2*reg*self.W
+        return loss,dW
+
+class Softmax(LinearClassifier):
+    def __init__(self):
+        super().__init__()
+    def loss(
+        self,
+        X,
+        y,
+        out,
+        reg : float = 1e-5,
+        ):
+        n,c = out.shape
+        out_ex = torch.exp(out)
+        den = torch.sum(out_ex,dim=1).reshape((-1,1)).expand((-1,c))
+        prob = out_ex/den
+        
+        logprob = -torch.log(prob[range(n),y])
+        loss = torch.sum(logprob)/n + reg*torch.sum(self.W.mul(self.W))
+
+        prob[range(n),y] -= 1
+        dW = torch.mm(X.T,prob)/n
+        dW += 2*reg*self.W
+
+        return loss,dW
---
+++ classifier/readme.md
@@ -0,0 +1,3 @@
+### CH3 이미지 분류를 위한 선형 분류기
+
+- 개념 설명 : [블로그 링크](https://rimiblog.oopy.io/4bfafde4-8bdf-4316-ab05-17fba918f630)
